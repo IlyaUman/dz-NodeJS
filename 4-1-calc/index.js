dfcf8824bffca7d1bcaee8616a030bbc558f3465
Ilya Uman <ilya.uman@gmail.com>
2024-01-13 20:36:02 +0300

    Corrected

--- 4-1-calc/index.js
+++ 4-1-calc/index.js
@@ -1,37 +1,24 @@
-import { add } from './add.js'
+import { add } from "./add.js";
 
-import { mult } from './mult.js'
+import { mult } from "./mult.js";
 
-import { div } from './div.js'
+import { div } from "./div.js";
 
-const operator = process.argv[2]
-
-if (Number(process.argv[2])) 
-	{
-		console.log('You have forgotten to type the operator first: add, mult or div')
-	} else
-if (isNaN(process.argv[3]) || isNaN(process.argv[4])) 
-	{
-		console.log('Type numbers!')
-	} else 
-{
-	switch(operator) {
-		case 'add': 
-			console.log(add(Number(process.argv[3]), Number(process.argv[4])))
-			break
-		case 'mult': 
-			console.log(mult(Number(process.argv[3]), Number(process.argv[4])))
-			break
-		case 'div':
-			if (process.argv[4] == 0) {
-				console.log ('Division by zero!')
-			} else {
-				console.log(div(Number(process.argv[3]), Number(process.argv[4])))
-			}
-			break
-		default:
-			console.log('Enter correct operator')
-			break
+const [, , operator, num1, num2] = process.argv;
+const operators = ['add', 'mult', 'div'];
+const func = {
+	'add': add,
+	'mult': mult,
+	'div': div
+};
+if (!operators.includes(operator)) {
+	console.error('Enter correct operator!');
+	//throw new Error('Enter correct operator!')	- почему-то не работает(((
+} else {
+	try {
+		console.log(func[operator](num1, num2))
+	} catch(e) {
+		//if (e instanceof )
+		console.log(e.message)
 	}
-	
-} 
+};
