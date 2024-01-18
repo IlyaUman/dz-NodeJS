cc9fa21002bca47712ea180f51f8b5cac1a03cd8
Ilya Uman <ilya.uman@gmail.com>
2024-01-18 13:57:06 +0300

    Second task about timer
    в качестве аргументов ожидается строка вида 1h 2m 3s или 2m 3s или 3s или пустая строка. Таймер сработает в любом случае не более чем через 5 секунд, сделано специально, чтобы можно было проверить ввод минут и часов и не ждать.

--- 4-2-timer/index.js
+++ 4-2-timer/index.js
@@ -1,12 +1,27 @@
+const [, , arg1, arg2, arg3] = process.argv;
+const secs =
+  arg3 != undefined
+    ? parseFloat(arg3)
+    : arg2 != undefined
+    ? parseFloat(arg2)
+    : parseFloat(arg1);
+const mins =
+  arg3 != undefined
+    ? parseFloat(arg2)
+    : arg2 != undefined
+    ? parseFloat(arg1)
+    : 0;
+const hours = arg3 != undefined ? parseFloat(arg1) : 0;
+const timer = secs * 1000 + mins * 60 * 1000 + hours * 360 * 1000;
+const stringForOutput = `${
+  hours + Math.floor(mins / 60) + Math.floor(secs / 3600)
+} hours, ${mins - Math.floor(mins / 60) * 60 + Math.floor(secs / 60)} mins, ${
+  secs - Math.floor(secs / 3600) * 3600 - Math.floor(secs / 60) * 60
+} secs`;
 
-const [, , arg1, arg2, arg3] = process.argv
-const secs = arg3 != undefined ? parseFloat(arg3) : (arg2 != undefined ? parseFloat(arg2) : parseFloat(arg1))
-const mins = arg3 != undefined ? parseFloat(arg2) : (arg2 != undefined ? parseFloat(arg1) : 0)
-const hours = arg3 != undefined ? parseFloat(arg1) : 0
-const timer = secs*1000 + mins*60*1000 + hours*360*1000
-const stringForOutput = `${hours + Math.floor(mins / 60) + Math.floor(secs / 3600)} hours, ${mins - Math.floor(mins / 60)*60 + Math.floor(secs / 60)} mins, ${secs - Math.floor(secs / 3600) * 3600 - Math.floor(secs / 60) * 60} secs`
-
-
-setTimeout(() => {
-	console.log(`Timer worked! You entered ${stringForOutput}`)
-}, timer <= 5000 ? timer : 5000)
+setTimeout(
+  () => {
+    console.log(`Timer worked! You entered ${stringForOutput}`);
+  },
+  timer <= 5000 ? timer : 5000
+);
