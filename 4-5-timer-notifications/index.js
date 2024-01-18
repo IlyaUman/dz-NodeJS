a96dd244bc025f39b610cc84567b4bd7d7fc8093
Ilya Uman <ilya.uman@gmail.com>
2024-01-18 13:37:20 +0300

    Notifier

--- 4-5-timer-notifications/index.js
+++ 4-5-timer-notifications/index.js
@@ -1,29 +1,35 @@
-import { WindowsToaster } from 'node-notifier'
+import { WindowsToaster } from "node-notifier";
 
-const notifier = new WindowsToaster()
+const notifier = new WindowsToaster();
 
-const start = performance.now()
+const [, , arg1, arg2, arg3] = process.argv;
+const secs =
+  arg3 != undefined
+    ? parseFloat(arg3)
+    : arg2 != undefined
+    ? parseFloat(arg2)
+    : parseFloat(arg1);
+const mins =
+  arg3 != undefined
+    ? parseFloat(arg2)
+    : arg2 != undefined
+    ? parseFloat(arg1)
+    : 0;
+const hours = arg3 != undefined ? parseFloat(arg1) : 0;
+const timer = secs * 1000 + mins * 60 * 1000 + hours * 360 * 1000;
+const stringForOutput = `${
+  hours + Math.floor(mins / 60) + Math.floor(secs / 3600)
+} hours, ${mins - Math.floor(mins / 60) * 60 + Math.floor(secs / 60)} mins, ${
+  secs - Math.floor(secs / 3600) * 3600 - Math.floor(secs / 60) * 60
+} secs`;
 
-let secs, mins, hours, timer
-
-if (!process.argv[2]) {
-	console.log('No delay for timer entered')
-} else if (!process.argv[3]) {
-	timer =  parseFloat(process.argv[2])*1000
-	secs = parseFloat(process.argv[2])
-} else if (!process.argv[4]) {
-	timer =  parseFloat(process.argv[3])*1000 + parseFloat(process.argv[2])*60*1000
-	mins = parseFloat(process.argv[2])
-	secs = parseFloat(process.argv[3])
+if (!secs) {
+  console.error("No delay for timer entered");
 } else {
-	timer =  parseFloat(process.argv[4])*1000 + parseFloat(process.argv[3])*60*1000 + parseFloat(process.argv[2])*3600*1000
-	hours = parseFloat(process.argv[2])
-	mins = parseFloat(process.argv[3])
-	secs = parseFloat(process.argv[4])
+  setTimeout(
+    () => {
+      notifier.notify(`Timer worked! You entered ${stringForOutput}`);
+    },
+    timer <= 5000 ? timer : 5000
+  );
 }
-
-
-setTimeout(() => {
-	notifier.notify(`Timer worked! You entered ${hours || 0} hours, ${mins || 0} mins, ${secs || 0} secs`)
-}, timer <= 5000 ? timer : 5000)
-
