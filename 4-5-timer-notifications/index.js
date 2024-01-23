81d279ca0d0b748868a1f75327190ec85a452ef3
Ilya Uman <ilya.uman@gmail.com>
2024-01-23 20:50:55 +0300

    Notifier

--- 4-5-timer-notifications/index.js
+++ 4-5-timer-notifications/index.js
@@ -1,35 +1,25 @@
 import { WindowsToaster } from "node-notifier";
+import {
+  getHours,
+  getMins,
+  getOutputString,
+  getSecs,
+  getTimer,
+} from "./getTime.js";
 
 const notifier = new WindowsToaster();
 
 const [, , arg1, arg2, arg3] = process.argv;
-const secs =
-  arg3 != undefined
-    ? parseFloat(arg3)
-    : arg2 != undefined
-    ? parseFloat(arg2)
-    : parseFloat(arg1);
-const mins =
-  arg3 != undefined
-    ? parseFloat(arg2)
-    : arg2 != undefined
-    ? parseFloat(arg1)
-    : 0;
-const hours = arg3 != undefined ? parseFloat(arg1) : 0;
-const timer = secs * 1000 + mins * 60 * 1000 + hours * 360 * 1000;
-const stringForOutput = `${
-  hours + Math.floor(mins / 60) + Math.floor(secs / 3600)
-} hours, ${mins - Math.floor(mins / 60) * 60 + Math.floor(secs / 60)} mins, ${
-  secs - Math.floor(secs / 3600) * 3600 - Math.floor(secs / 60) * 60
-} secs`;
+const time = {
+  secs: getSecs([arg1, arg2, arg3]),
+  mins: getMins([arg1, arg2, arg3]),
+  hours: getHours([arg1, arg2, arg3]),
+};
 
-if (!secs) {
-  console.error("No delay for timer entered");
-} else {
-  setTimeout(
-    () => {
-      notifier.notify(`Timer worked! You entered ${stringForOutput}`);
-    },
-    timer <= 5000 ? timer : 5000
-  );
-}
+const timer = getTimer(time.secs, time.mins, time.hours);
+setTimeout(
+  () => {
+    notifier.notify(getOutputString(time.secs, time.mins, time.hours));
+  },
+  timer <= 2000 ? timer : 1000
+);
