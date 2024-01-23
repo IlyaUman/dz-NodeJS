8ac78ed3d9c097e8a8c5139f41ab4aea61a7b55d
Ilya Uman <ilya.uman@gmail.com>
2024-01-23 21:13:04 +0300

    Second task about timer
    в качестве аргументов ожидается строка вида 1h 2m 3s или 2m 3s или 3s или пустая строка. Таймер сработает в любом случае не более чем через 1 секундe, сделано специально, чтобы можно было проверить ввод минут и часов и не ждать.

--- 4-2-timer/index.js
+++ 4-2-timer/index.js
@@ -1,27 +1,22 @@
-const [, , arg1, arg2, arg3] = process.argv;
-const secs =
-  arg3 != undefined
-    ? parseFloat(arg3)
-    : arg2 != undefined
-    ? parseFloat(arg2)
-    : parseFloat(arg1);
-const mins =
-  arg3 != undefined
-    ? parseFloat(arg2)
-    : arg2 != undefined
-    ? parseFloat(arg1)
-    : 0;
-const hours = arg3 != undefined ? parseFloat(arg1) : 0;
-const timer = secs * 1000 + mins * 60 * 1000 + hours * 360 * 1000;
-const stringForOutput = `${
-  hours + Math.floor(mins / 60) + Math.floor(secs / 3600)
-} hours, ${mins - Math.floor(mins / 60) * 60 + Math.floor(secs / 60)} mins, ${
-  secs - Math.floor(secs / 3600) * 3600 - Math.floor(secs / 60) * 60
-} secs`;
+import {
+  getHours,
+  getMins,
+  getOutputString,
+  getSecs,
+  getTimer,
+} from "./getTime.js";
 
+const [, , arg1, arg2, arg3] = process.argv;
+const time = {
+  secs: getSecs([arg1, arg2, arg3]),
+  mins: getMins([arg1, arg2, arg3]),
+  hours: getHours([arg1, arg2, arg3]),
+};
+
+const timer = getTimer(time.secs, time.mins, time.hours);
 setTimeout(
   () => {
-    console.log(`Timer worked! You entered ${stringForOutput}`);
+    console.log(getOutputString(time.secs, time.mins, time.hours));
   },
-  timer <= 5000 ? timer : 5000
+  timer <= 2000 ? timer : 1000
 );
